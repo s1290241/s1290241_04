4e4eae9f9c96df8a91ddfdf4955475c86ad2bc9f
s1290241 <s1290241@call1dc9.u-aizu.ac.jp>
2022-07-13 15:08:34 +0900

    HoT: initial revision

--- coin00.c
+++ coin00.c
@@ -3,10 +3,6 @@
 #include<time.h>
 int main()
 {
-  char str[100];
-  printf("Who are you?\n");
-  scanf("%s",str);
-  printf("Hello,%s!\n",str);
   int coin[3];
   int h=0;
   int t=0;
@@ -32,14 +28,6 @@ int main()
   
 
   printf("Heads: %d, Tails: %d\n",h,t);
-  if(h>t)
-  {
-    printf("you won!\n");
-  }
-  else
-  {
-    printf("you lost!\n");
-  }
-  
+ 
 
 }
